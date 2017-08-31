4754bde5e9fd0b4fd412d575a9f664ad19bdf50d
Philippe Granger <pgranger@elementai.com>
2017-08-31 16:05:09 -0400

    Add git bisect

--- test_16.py
+++ test_16.py
@@ -1,4 +1,23 @@
 """
+bonus git bisect
+
+create file test.py
+
+
+import fixme
+
+def test_fixme():
+    assert fixme.plusone(5) == 6
+
+find first commit: 7d28e0ace912ee3cd2565df553d944349aa8aa4d
+
+git bisect start HEAD 7d28e0ace912ee3cd2565df553d944349aa8aa4d
+git bisect run pytest test.py
+git bisect reset
+
+"""
+
+"""
 https://www.youtube.com/watch?v=DJtef410XaM
 The clean architecture
 """
